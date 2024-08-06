e386b203459559526636d7711b11bc906566d09e
Brandon Bane <brandon.bane17@gmail.com>
2024-08-05 22:40:55 -0400

    chat streaming functional

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -34,10 +34,28 @@ export async function POST(req) {
     const data = await req.json()
     const completion = await openai.chat.completions.create({
         messages: [
-            {role: 'system', content: systemPrompt},
-        ...data],
-        model: "gpt-3.5-turbo",
+            {role: 'system', content: systemPrompt}, ...data],
+        model: "gpt-4o",
+        stream: true,
     })
 
-    return NextResponse.json({message: completion.choices[0].message.content}, {status: 200})
+    const stream = new ReadableStream({
+        async start(controller) {
+            const encoder = new TextEncoder()
+            try {
+                for await (const chunk of completion) {
+                    const content = chunk.choices[0]?.delta?.content
+                    if (content) {
+                        const text = encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                }
+            } catch (err) {
+                controller.error(err)
+            } finally {
+                controller.close()
+            }
+        },
+    })
+    return new NextResponse(stream)
 }
--- app/page.js
+++ app/page.js
@@ -6,20 +6,35 @@ export default function Home() {
   const [messages, setMessages] = useState([
     {role: 'assistant', content: 'Hello! I am the basketball stats chatbot. How can I help you today?'},
   ])
+  const [message, setMessage] = useState('')
   const sendMessage = async () => {
     setMessage('')
-    setMessages((messages) => [...messages, {role: 'user', content: message}])
-    const response = await fetch('/api/chat', {
+    setMessages((messages) => [...messages, {role: 'user', content: message}, {role: 'assistant', content: ''}])
+    const response = fetch('/api/chat', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify([...messages, {role: 'user', content: message}]),
+      }).then(async (res) => {
+        const reader = res.body.getReader();
+        const decoder = new TextDecoder();
+        let result = '';
+        return reader.read().then(function processText({ done, value }) {
+          if (done) {
+            return result
+          }
+          const text = decoder.decode(value || new Uint8Array(), { stream: true });
+          setMessages((messages) => {
+            let lastMessage = messages[messages.length - 1]
+            let otherMessages = messages.slice(0, messages.length - 1)
+
+            return [...otherMessages, {...lastMessage, content: lastMessage.content + text}];
+          })
+          return reader.read().then(processText)
+        })
       })
-      const data = await response.json()
-      setMessages((messages) => [...messages, {role: 'assistant', content: data.message}])
     }
-  const [message, setMessage] = useState('')
   return (
     <Box
       width="100vw"
